80ca63ce9d2661e67fc5d67fd8e8ad60d805c65a
WayneJN <waynejnorman@gmail.com>
2025-02-09 22:17:42 -0330

    Update to query page, and seperate requests

---
+++ Queries/query1.sql
@@ -0,0 +1,21 @@
+-- Retrieve the names and stock quantities of all products
+
+
+--Step method
+-- Step 1: Select all products
+WITH all_products AS (
+    SELECT id, product_name, stock_quantity
+    FROM products
+)
+
+-- Step 2: Select the product names and stock quantities
+SELECT product_name, stock_quantity
+FROM all_products;
+
+
+
+--Simplified view
+SELECT product_name, stock_quantity
+FROM products;
+
+
