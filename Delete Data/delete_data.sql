f8c86865800a1cf6ea9a7aa03bbe4af746ab8ab9
WayneJN <waynejnorman@gmail.com>
2025-02-09 22:52:28 -0330

    Delete Delete Data directory
    Accidentally created 2 copies

--- Delete Data/delete_data.sql
+++
@@ -1,7 +0,0 @@
--- Remove one of the orders and all associated order items from the system
--- Let's remove order_id = 1
-DELETE FROM order_items
-WHERE order_id = 1;
-
-DELETE FROM orders
-WHERE id = 1;
